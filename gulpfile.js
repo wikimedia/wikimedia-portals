8539f251dbb9e21eae5adfccca106d9ed1a37d2f
Julien Girault <jgirault@wikimedia.org>
2015-10-29 19:35:01 -0700

    Portals code should follow the same coding conventions as MW
    - Added .jscsrc from wikimedia/core
    - Added .jshint from wikimedia/core
    - Added gulp-jscs to package.json
    - Added jscs validator to gulp lint
    - Fixed code style of gulpfile.js
    - Fixed code style of wm-portal.js
    
    Bug: T116979
    Change-Id: I8b58ac72c66dd8f81b6c46360829241e8e2b8357

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,63 @@
-var gulp = require('gulp'),
-    gulpLoadPlugins = require('gulp-load-plugins');
+/* jshint strict:false */
+/* globals require */
+/* globals process */
+/* globals console */
+var gulp = require( 'gulp' ),
+	gulpLoadPlugins = require( 'gulp-load-plugins' ),
+	argv = require( 'yargs' ).argv,
+	imageminPngquant = require( 'imagemin-pngquant' );
 
-var argv = require('yargs').argv;
+var plugins = gulpLoadPlugins(),
+	portalParam = argv.portal;
 
-var plugins = gulpLoadPlugins();
-
-var imageminPngquant = require('imagemin-pngquant');
-
-var portalParam = argv.portal;
-
-if (!portalParam && process.argv[2] !== "help") {
-  console.log("\x1b[31m");
-  console.log("Error: please specify the portal you wish to build.");
-  console.log("Type gulp help for more information.");
-  console.log("\x1b[0m");
-  process.exit(1);
+if ( !portalParam && process.argv[ 2 ] !== 'help' ) {
+	console.log( '\x1b[31m' );
+	console.log( 'Error: please specify the portal you wish to build.' );
+	console.log( 'Type gulp help for more information.' );
+	console.log( '\x1b[0m' );
+	process.exit( 1 );
 }
 
-var baseDir = "dev/"+portalParam+"/";
-var prodDir = "prod/"+portalParam+"/";
+var baseDir = 'dev/' + portalParam + '/',
+	prodDir = 'prod/' + portalParam + '/';
 
-gulp.task('help', function () {
-  console.log();
-  console.log('+-------------------------------------------------------------------------------------------------+');
-  console.log('|                                     =====  USAGE     =====                                      |');
-  console.log('+-------------------------------------------------------------------------------------------------+');
-  console.log('| gulp inline-assets --portal wikipedia.org   - build inline CSS and JS assets                    |');
-  console.log('| gulp optimize-images --portal wikipedia.org - run imagemin on image directory                   |');
-  console.log('| gulp lint --portal wikipedia.org            - run jslint on JS files on portal                  |');
-  console.log('| gulp --portal wikipedia.org                 - run all of the above on the specified portal page |');
-  console.log('+-------------------------------------------------------------------------------------------------+');
-  console.log();
-});
+gulp.task( 'help', function () {
+	console.log();
+	console.log( '+-------------------------------------------------------------------------------------------------+' );
+	console.log( '|                                     =====  USAGE     =====                                      |' );
+	console.log( '+-------------------------------------------------------------------------------------------------+' );
+	console.log( '| gulp inline-assets --portal wikipedia.org   - build inline CSS and JS assets                    |' );
+	console.log( '| gulp optimize-images --portal wikipedia.org - run imagemin on image directory                   |' );
+	console.log( '| gulp lint --portal wikipedia.org            - run jslint on JS files on portal                  |' );
+	console.log( '| gulp --portal wikipedia.org                 - run all of the above on the specified portal page |' );
+	console.log( '+-------------------------------------------------------------------------------------------------+' );
+	console.log();
+} );
 
-gulp.task('inline-assets', function(){
-  gulp.src(baseDir+"index.html")
-      .pipe(plugins.inline({
-        base: baseDir,
-        js: plugins.uglify,
-        css: plugins.minifyCss,
-        disabledTypes: ["svg", "img"]
-      }))
-      .pipe(gulp.dest(prodDir));
-});
+gulp.task( 'inline-assets', function () {
+	gulp.src( baseDir + 'index.html' )
+		.pipe( plugins.inline( {
+			base: baseDir,
+			js: plugins.uglify,
+			css: plugins.minifyCss,
+			disabledTypes: [ 'svg', 'img' ]
+		} ) )
+		.pipe( gulp.dest( prodDir ) );
+} );
 
-gulp.task('optimize-images', function(){
-  gulp.src(baseDir+"assets/img/*")
-      .pipe(imageminPngquant({quality: '80-95', speed: 1})())
-      .pipe(plugins.imagemin())
-      .pipe(gulp.dest(prodDir+"assets/img"));
-});
+gulp.task( 'optimize-images', function () {
+	gulp.src( baseDir + 'assets/img/*' )
+		.pipe( imageminPngquant( { quality: '80-95', speed: 1 } )() )
+		.pipe( plugins.imagemin() )
+		.pipe( gulp.dest( prodDir + 'assets/img' ) );
+} );
 
-gulp.task('lint', function(){
-  gulp.src(baseDir+"assets/js/*.js")
-      .pipe(plugins.jshint(".jshintrc"))
-      .pipe(plugins.jshint.reporter('default'));
-});
+gulp.task( 'lint', function () {
+	gulp.src( [ 'gulpfile.js', baseDir + 'assets/js/*.js' ] )
+		.pipe( plugins.jshint( '.jshintrc' ) )
+		.pipe( plugins.jshint.reporter( 'default' ) )
+		.pipe( plugins.jscs() )
+		.pipe( plugins.jscs.reporter() );
+} );
 
-gulp.task('default', ['lint', 'inline-assets', 'optimize-images']);
+gulp.task( 'default', [ 'lint', 'inline-assets', 'optimize-images' ] );
