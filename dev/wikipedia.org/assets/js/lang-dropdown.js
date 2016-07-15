84feffc2ec21fe1e060bf3ffb6e232ab3e259bf0
Jan Drewniak <jan.drewniak@gmail.com>
2016-07-15 17:12:16 +0200

    New layout with language list dropdown.
    As per the conversation on
    https://www.mediawiki.org/wiki/Talk:Wikipedia.org_updated_page_layout
    
    Bug: T140153
    Change-Id: I7c44b270a53d3a7916950e91ab76995632f8a0a1

---
+++ dev/wikipedia.org/assets/js/lang-dropdown.js
@@ -0,0 +1,52 @@
+/* global addEvent, mw, wmTest */
+/*
+ * The following functions open and close the language list
+ * via the language drop-down button.
+ *
+ * - If the user has JS disabled, the language list is open by default.
+ * - If the users browser is set to a language that is not present on the page,
+ *   the list also opens.
+ * - When the user opens the list and refreshes the page, the list remains open
+ *   (provided their browser supports localStorage).
+ *
+ */
+
+( function () {
+
+	var langButton = document.getElementById( 'js-lang-list-button' );
+
+	function toggleActiveClass() {
+		if ( / lang-list-active /g.test( document.body.className ) ) {
+			document.body.className = document.body.className.replace( ' lang-list-active ', '' );
+			mw.storage.set( 'lang-list-active', 'false' );
+		} else {
+			document.body.className += ' lang-list-active ';
+			mw.storage.set( 'lang-list-active', 'true' );
+		}
+	}
+
+	function userLangWikiMissing( langs ) {
+		var anchors = document.getElementsByTagName( 'a' ),
+			langMissing = true; // being pessimistic
+
+		for ( var i = 0; i < anchors.length && langMissing; i++ ) {
+			var anchor = anchors[ i ],
+				langAttr = anchor.getAttribute( 'lang' );
+
+			if ( langAttr && langs.indexOf( langAttr ) >= 0 ) {
+				langMissing = false; // lang exists
+			}
+		}
+
+		return langMissing;
+	}
+
+	if ( mw.storage.get( 'lang-list-active' ) === 'true' || userLangWikiMissing( wmTest.userLangs ) ) {
+		toggleActiveClass();
+	}
+
+	addEvent( langButton, 'click', function () {
+		toggleActiveClass();
+	} );
+
+} )();
