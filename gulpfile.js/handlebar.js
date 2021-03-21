cdf8b4741fe42f173c76aaa21e62bbe9296caf7d
bharatkhatri351 <bharatkhatri351@gmail.com>
2021-03-21 22:13:25 +0530

    Adds handlebar.js file in gulpfile.js directory
    Added function compilehandlebars in handlebar.js and remove from index.js
    
    Bug: T277405
    Change-Id: Ieecf3e918d79f27b753f477d682dbdb7289b69fc

---
+++ gulpfile.js/handlebar.js
@@ -0,0 +1,18 @@
+var gulp = require( 'gulp' ),
+gulpLoadPlugins = require( 'gulp-load-plugins' ),
+plugins = gulpLoadPlugins();
+
+const { requirePortalParam, getBaseDir, getConfig } = require( './config' );
+
+function compileHandlebars() {
+
+	requirePortalParam();
+
+	return gulp.src( getConfig().hb.src )
+		.pipe( plugins.compileHandlebars( getConfig().hb.templateData, getConfig().hb.options ) )
+		.pipe( plugins.rename( 'index.html' ) )
+		.pipe( gulp.dest( getBaseDir() ) );
+}
+gulp.task( 'compile-handlebars', compileHandlebars );
+
+exports.compileHandlebars = compileHandlebars;
