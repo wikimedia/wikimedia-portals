f88ed927ed535d233193c77a7036c9fac23a1961
Ishan-Saini <ishansaini243@gmail.com>
2021-03-21 18:17:42 +0530

    Adds config.js file in gulpfile.js directory
    Various functions like requirePortalParam(), getBaseDir(), getProdDir(),
    getConfig() have been moved from index.js to config.js file in gulpfile.js
    directory for refactoring purposes.
    
    Bug: T277405
    Change-Id: I560a85ac93ea53eccf1d4279b9aa32484999b0ef

---
+++ gulpfile.js/config.js
@@ -0,0 +1,98 @@
+var argv = require( 'yargs' ).argv,
+	portalParam = argv.portal,
+	getBaseDir, getProdDir, getConfig;
+
+/**
+ * Preliminary task for tasks that require the portal param.
+ * Also sets config for remaining tasks.
+ */
+
+function requirePortalParam() {
+	if ( !portalParam ) {
+		console.log( '\x1b[31m' );
+		console.log( 'Error: please specify the portal you wish to build.' );
+		console.log( 'Type gulp help for more information.' );
+		console.log( '\x1b[0m' );
+		process.exit( 1 );
+	}
+}
+
+getBaseDir = function () {
+	requirePortalParam();
+
+	getBaseDir = function () {
+		return 'dev/' + portalParam + '/';
+	};
+	return getBaseDir();
+};
+
+getProdDir = function () {
+	requirePortalParam();
+
+	getProdDir = function () {
+		return 'prod/' + portalParam + '/';
+	};
+	return getProdDir();
+};
+
+getConfig = function () {
+	var config = {},
+		baseDir, prodDir;
+
+	baseDir = getBaseDir();
+	prodDir = getProdDir();
+
+	config.hb = {
+		src: baseDir + 'index.handlebars',
+		templateData: require( '../' + baseDir + 'controller.js' ),
+		options: {
+			batch: [ './' + baseDir + '/templates' ],
+			helpers: require( '../hbs-helpers.global' )
+		}
+	};
+
+	config.htmlmin = {
+		src: prodDir + 'index.html',
+		dest: prodDir + 'index.html',
+		options: {
+			preventAttributesEscaping: true,
+			collapseWhitespace: true,
+			preserveLineBreaks: true,
+			collapseBooleanAttributes: false
+		}
+	};
+
+	config.watch = {
+		sprites: [ baseDir + 'assets/img/sprite_assets/**/*' ],
+		postcss: baseDir + 'assets/postcss/*.css',
+		hb: [ baseDir + '*.handlebars',
+			baseDir + '.json',
+			baseDir + 'controller.js',
+			baseDir + 'templates/**/*'
+		]
+	};
+
+	config.img = {
+		src: [ baseDir + 'assets/img/*', '!' + baseDir + 'assets/img/sprite_assets' ],
+		dest: prodDir + 'assets/img',
+		sprite: {
+			cssPrefix: 'sprite',
+			assets: baseDir + 'assets/img/sprite_assets/*.svg',
+			outputName: 'sprite',
+			outputCSS: 'sprite.css',
+			outputCSSPath: baseDir + 'assets/css/sprite.css',
+			outputSVGGlob: baseDir + 'assets/img/sprite*.svg',
+			template: baseDir + 'assets/css/sprite-template.mustache'
+		}
+	};
+
+	getConfig = function () {
+		return config;
+	};
+	return getConfig();
+};
+
+exports.requirePortalParam = requirePortalParam;
+exports.getBaseDir = getBaseDir;
+exports.getProdDir = getProdDir;
+exports.getConfig = getConfig;
