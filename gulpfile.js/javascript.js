77e2bae582fe800bca16b7e0ce8d604c785afdf6
Ishan-Saini <ishansaini243@gmail.com>
2021-03-30 09:55:47 +0530

    Adds javascript.js file in gulpfile.js directory
    Shifted lintJS() function from index.js to javascript.js file for
    refactoring purpose.
    
    Bug: T277405
    Change-Id: Ib7d4b40a6ea9156f02bd9b8ec2fedca14c3c3b9d

---
+++ gulpfile.js/javascript.js
@@ -0,0 +1,19 @@
+var gulp = require( 'gulp' ),
+	gulpLoadPlugins = require( 'gulp-load-plugins' ),
+	plugins = gulpLoadPlugins(),
+	argv = require( 'yargs' ).argv,
+	portalParam = argv.portal;
+
+function lintJS() {
+	var srcFolder = 'src/**/*.js';
+	if ( portalParam ) {
+		// Only run on this portal files.
+		srcFolder = 'src/' + portalParam + '/**/*.js';
+	}
+	return gulp.src( [ '*.js', srcFolder ] )
+		.pipe( plugins.eslint7() )
+		.pipe( plugins.eslint7.format() )
+		.pipe( plugins.eslint7.failAfterError() );
+}
+
+exports.lintJS = lintJS;
