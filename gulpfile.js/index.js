7a4c9a5678643ad5916f57f7a4ceb45c8b9ce6ff
Ishan-Saini <ishansaini243@gmail.com>
2021-04-21 22:13:38 +0530

    Add meta.js file in gulpfile.js directory
    Moved fetchMeta() function from index.js file to meta.js in gulpfile.js
    directory.
    
    Bug: T277405
    Change-Id: I92be047b6b1d42c8a1295d9efd99662758815e9e

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -67,41 +67,8 @@ const { updateStats } = require( './stats' );
 
 gulp.task( 'update-stats', updateStats );
 
-function fetchMeta() {
+const { fetchMeta } = require( './meta' );
 
-	var portalsFromMeta,
-		portalRequest,
-		portalRequests = [];
-
-	requirePortalParam();
-
-	if ( portalParam === 'wikipedia.org' ) {
-		console.log( 'Cannot override ' + portalParam + ' portal using fetch-meta.' );
-		return process.exit( 1 );
-	}
-
-	if ( portalParam === 'all' ) {
-		portalsFromMeta = [ 'wikibooks.org', 'wikimedia.org', 'wikinews.org', 'wikiquote.org', 'wikiversity.org', 'wikivoyage.org', 'wiktionary.org' ];
-
-		portalsFromMeta.forEach( function ( wiki ) {
-			var request = preq.get( 'https://meta.wikimedia.org/w/index.php?title=Www.' + wiki + '_template&action=raw' )
-				.then( function ( response ) {
-					fs.mkdirSync( 'prod/' + wiki, { recursive: true } );
-					return fs.writeFileSync( 'prod/' + wiki + '/index.html', response.body, 'utf8' );
-				} );
-			portalRequests.push( request );
-		} );
-	} else {
-		portalRequest = preq.get( 'https://meta.wikimedia.org/w/index.php?title=Www.' + portalParam + '_template&action=raw' )
-			.then( function ( response ) {
-				fs.mkdirSync( 'prod/' + portalParam, { recursive: true } );
-				return fs.writeFileSync( 'prod/' + portalParam + '/index.html', response.body, 'utf8' );
-			} );
-		portalRequests.push( portalRequest );
-	}
-
-	return Promise.all( portalRequests );
-}
 gulp.task( 'fetch-meta', fetchMeta );
 
 /**
