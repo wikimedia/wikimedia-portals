e8f98606443f7c0b21ecd356c0b61dcbdf81e5b6
bharatkhatri351 <bharatkhatri351@gmail.com>
2021-07-20 14:39:09 +0530

    javascript integration
    Created portal specific js files "wm-test.js" and embed required javascript files in "index.handlebars" for full fledge working of
    wikitionary portal.Also  Fixed some issue of last patch.
    
    Bug: T287324
    Change-Id: I60c99a3818d9381989d8185855a034e3bb71689c

--- src/wiktionary.org/controller.js
+++ src/wiktionary.org/controller.js
@@ -1,7 +1,15 @@
 /* eslint-env node, es6 */
-var Controller,
+var _ = require( 'underscore' ),
+	hbs = require( '../../hbs-helpers.global.js' ),
+	Controller,
 	glob = require( 'glob' ),
+	stats = require( '../../data/stats' ),
+	top100000List,
+	top100000Dropdown,
 	otherProjects = require( './other-projects.json' ),
+	siteStats,
+	range,
+	rtlLanguages = require( './rtl-languages.json' ),
 	l10n = require( '../../l10n/en.json' ); // These will be global values
 
 // This is specific to Wiktionary.
@@ -40,7 +48,54 @@ function getPreloadLinks() {
 	return preloadLinks;
 }
 
+// Format the dropdown for ./templates/search.mustache
+top100000List = stats.getRange( 'wiktionary', 'numPages', 100000 );
+top100000Dropdown = stats.format( 'wiktionary', top100000List, {
+	stripTags: true
+} );
+
+/**
+ *  SiteStats returns and object for each language wiktionary by language code.
+ *  ex:
+ *  "en":{"url":"en.wiktionary.org",
+ *        "numPages":"5 077 000",
+ *        "views":1723574042,
+ *        "siteName":"Wiktionary",
+ *        "articles":"articles",
+ *        "slogan":"The Free Encyclopedia",
+ *        "name":"English",
+ *        "lang":"en"
+ *        }
+ */
+siteStats = {};
+range = stats.getRangeFormatted( 'wiktionary', 'views', 10 );
+
+_.each( range, function ( wiki ) {
+	if ( wiki.closed || wiki.sublinks ) {
+		return;
+	}
+
+	wiki.numPages = hbs.formatNumber( wiki.numPages, {
+		hash: {
+			thousandSeparator: true,
+			rounded: true,
+			nbsp: false
+		}
+	} ).toString();
+
+	siteStats[ wiki.code ] = _.omit( wiki, 'closed', 'code', 'index' );
+} );
+
 Controller = {
+	top10views: stats.getTopFormatted( 'wiktionary', 'views', 10 ),
+	top1000000Articles: stats.getRangeFormatted( 'wiktionary', 'numPages', 1000000 ),
+	top100000Articles: stats.getRangeFormatted( 'wiktionary', 'numPages', 100000, 1000000 ),
+	top10000Articles: stats.getRangeFormatted( 'wiktionary', 'numPages', 10000, 100000 ),
+	top1000Articles: stats.getRangeFormatted( 'wiktionary', 'numPages', 1000, 10000 ),
+	top100Articles: stats.getRangeFormatted( 'wiktionary', 'numPages', 100, 1000 ),
+	top100000Dropdown: top100000Dropdown,
+	rtlLanguages: rtlLanguages,
+	rtlLanguagesStringified: '[\'' + rtlLanguages.join( '\',\'' ) + '\']',
 	preloadLinks: getPreloadLinks(),
 	otherProjects: otherProjects,
 	l10n
