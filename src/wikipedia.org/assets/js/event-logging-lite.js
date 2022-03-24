1320a6207591453bad204dc465ec89e497fb0e34
Jan Drewniak <jan.drewniak@gmail.com>
2022-03-23 21:00:18 -0400

    Followup 246c37b7, event-logging sends data in POST body not as URL params
    Following up on "Migrate wikipedia.org analytics to Event Platform".
    Previously the analytics sendbeacon encoded the event payload as
    URL query parameters. For the Event Platform migration, the data has to
    be sent in the request body (the second parameter in `navigator.sendbeacon()` ).
    
    This patch ensures that happens and removes the img fallback for
    event-logging since that method is not supported by the Event Platform.
    
    Bug: T282012
    Change-Id: I28c972238920d3726db51348371f93544d8a13bc

--- src/wikipedia.org/assets/js/event-logging-lite.js
+++ src/wikipedia.org/assets/js/event-logging-lite.js
@@ -60,22 +60,6 @@
 		byteToHex: byteToHex,
 
 		/**
-		 * Checks whether a beacon url is short enough,
-		 * so that it does not get truncated by varnishncsa.
-		 *
-		 * @param {string} schemaName Canonical schema name.
-		 * @param {string} url Beacon url.
-		 * @return {string|undefined} The error message in case of error.
-		 */
-		checkUrlSize: function ( schemaName, url ) {
-			var message;
-			if ( url.length > self.maxUrlSize ) {
-				message = 'Url exceeds maximum length';
-				return message;
-			}
-		},
-
-		/**
 		 * Generate a random user session ID.
 		 *
 		 * This information would potentially be stored in a cookie to identify a user during a
@@ -208,38 +192,20 @@
 		},
 
 		/**
-		 * Constructs the EventLogging URI based on the base URI and the
-		 * encoded and stringified data.
-		 *
-		 * @param {Object} data Payload to send
-		 * @return {string|boolean} The URI to log the event.
-		 */
-		makeBeaconUrl: function ( data ) {
-			var queryString = encodeURIComponent( JSON.stringify( data ) );
-			return baseUrl + '?' + queryString + ';';
-		},
-
-		/**
 		 * Transfer data to a remote server by making a lightweight HTTP
-		 * request to the specified URL.
+		 * POST request to the specified URL.
 		 *
 		 * If the user expressed a preference not to be tracked, or if
 		 * $wgEventLoggingBaseUri is unset, this method is a no-op.
 		 *
-		 * @param {string} url URL to request from the server.
+		 * @param {string} url URL to send the request.
+		 * @param {string} data Stringified JSON object containing all event data.
 		 */
-		sendBeacon: ( /1|yes/.test( navigator.doNotTrack ) || !baseUrl ) ?
+		sendBeacon: ( /1|yes/.test( navigator.doNotTrack ) || !baseUrl || !navigator.sendBeacon ) ?
 			helpers.noop :
-			navigator.sendBeacon ?
-				function ( url ) {
-					try {
-						navigator.sendBeacon( url );
-					} catch ( e ) {
-					}
-				} :
-				function ( url ) {
-					document.createElement( 'img' ).src = url;
-				},
+			function ( url, data ) {
+				navigator.sendBeacon( url, JSON.stringify( data ) );
+			},
 
 		/**
 		 * Construct and transmit to a remote server a record of some event
@@ -252,15 +218,8 @@
 		 * @param {Object} eventData Event object.
 		 */
 		logEvent: function ( schemaName, eventData ) {
-			var event = self.prepare( schemaName, eventData ),
-				url = self.makeBeaconUrl( event ),
-				sizeError = self.checkUrlSize( schemaName, url );
-
-			if ( !sizeError ) {
-				self.sendBeacon( url );
-			}
+			var event = self.prepare( schemaName, eventData );
+			self.sendBeacon( baseUrl, event );
 		}
-
 	}; // EventLoggingLite
-
 }() );
